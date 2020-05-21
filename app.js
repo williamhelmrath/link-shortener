96ea21973b2f6deccc2cce00a962476718262a55
David Dimmett <dcd2sg@virginia.edu>
2020-05-21 09:43:38 -0400

    link posting

--- app.js
+++ app.js
@@ -4,11 +4,27 @@ const Op = Sequelize.Op;
 const express = require("express");
 const app = express();
 const { Link } = require("./sequelize");
+const crypto = require("crypto");
+app.use(express.json());
 
-// run();
 app.post("/shorten/", (req, res) => {
   const { url } = req.body;
   console.log(`shortening ${url}`);
+  const hash = crypto.createHash("sha1");
+  hash.update(url);
+  let shortened = hash.digest("base64").substring(0, 10);
+  console.log(shortened);
+  Link.create({ original_link: url, shortened_link: shortened })
+    .then((newLink) => {
+      console.log(newLink);
+      res.status(200);
+      res.json(newLink);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500);
+      res.json(err);
+    });
 });
 
 app.get("/:hash", async (req, res) => {
