fc35ce85a8bdad4b054dd3627d71530389a5a869
William <wph2tu@virginia.edu>
2020-05-21 10:27:32 -0400

    added navbar and instructions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import SearchBar from "./components/SearchBar";
-function App() {
+import Navbar from "./components/Navbar";
+import Paper from "@material-ui/core/Paper";
+import "./App.css";
+
+export default function App() {
   const [postResp, setPostResp] = useState(null);
 
   const handleShorten = (link) => {
@@ -21,23 +25,25 @@ function App() {
         console.log(err);
       });
   };
-  if (!postResp)
-    return (
-      <div style={{ textAlign: "center" }}>
-        <h1>Shorten your link!</h1>
-        <SearchBar handleShorten={handleShorten} />
-      </div>
-    );
-  console.log(postResp);
+
   return (
     <div>
-      <div style={{ textAlign: "center" }}>
-        <h1>Shorten your link!</h1>
-        <SearchBar handleShorten={handleShorten} />
+      <Navbar />
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Paper variant="outlined" style={{ maxWidth: "40vw" }}>
+          Have a long-ass link that you need to shorten? Let Bro Shortener do
+          the heavy lifting for you!
+        </Paper>
+        <div>
+          <h1>Shorten your link!</h1>
+          <SearchBar handleShorten={handleShorten} />
+        </div>
       </div>
-      <h1>Your new link is: {postResp.shortened_link}</h1>
+      {postResp && (
+        <h1>
+          Your new link is: broshortener.herokuapp.com/{postResp.shortened_link}
+        </h1>
+      )}
     </div>
   );
 }
-
-export default App;
